562b74058c01ce70b7072abb80827a497835a74b
MunaFauziahAzZahra <144103689+MunaFauziahAzZahra@users.noreply.github.com>
2025-07-22 00:03:49 +0700

    add tulisan

--- src/pages/kontak/Kontak.jsx
+++ src/pages/kontak/Kontak.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Title from './components/TitleDescription'
 
 export default function Kontak() {
   return (
-    <div>Kontak</div>
+    <>
+    <Title mini_title="Kontak Kami" title="Kami ingin mendengar kabar dari Anda" description1="Jika Anda memiliki pertanyaan terkait aplikasi keuangan kami, jangan ragu untuk menghubungi kami." description2="info@gmail.com"/> 
+    </>
   )
 }
