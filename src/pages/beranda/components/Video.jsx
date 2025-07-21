774e2a895a1f097225d51327e7f59044bf09c261
MunaFauziahAzZahra <144103689+MunaFauziahAzZahra@users.noreply.github.com>
2025-07-21 23:01:48 +0700

    add video

--- src/pages/beranda/components/Video.jsx
+++ src/pages/beranda/components/Video.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Video_Protect from '../../assets/Videos/Video_Protect.mp4';
 
-export default function Video() {
+export default function Video(width, height) {
   return (
-    <div>Videoooooooooooo</div>
+    <div>
+        <video src={Video_Protect}
+        width={width}
+        height={height}
+        autoPlay
+        />
+    </div>
   )
 }
