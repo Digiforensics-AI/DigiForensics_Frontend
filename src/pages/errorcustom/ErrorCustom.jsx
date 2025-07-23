d3069f4c10dc588ffa648d13a4392a4e3a51513d
MunaFauziahAzZahra <144103689+MunaFauziahAzZahra@users.noreply.github.com>
2025-07-23 22:45:37 +0700

    edit errorpage

--- src/pages/errorcustom/ErrorCustom.jsx
+++ src/pages/errorcustom/ErrorCustom.jsx
@@ -6,7 +6,7 @@ import Title from './components/TitleDescription'
 export default function Error() {
   return (
     <>
-    <Image width={100} height={100} image={ErrorCustom}/>
+    <Image width={603} height={220} src={ErrorCustom}/>
     <Title title="Ups! Halaman Tidak Ditemukan." description="Tim ahli kami berkomitmen memahami kebutuhan Anda dan bekerja sama 
 untuk mengembangkan serta menerapkannya secara optimal."/>
 
